8dfa8b0a628b387d395d7701749ff397997d4aba
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-11-05 20:01:55 -0500

    [add] help command

---
+++ bin/commands.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ================================================
+powershell -Command "Write-Host '          Welcome to Custom Scripts!' -ForegroundColor Cyan"
+echo ================================================
+
+echo.
+echo  Commands available:
+echo.
+
+echo  example-command ^<parameter^>
+echo    - This command does something
+echo.
+
+echo ================================================
