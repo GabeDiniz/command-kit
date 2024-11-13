d9e8c2ce7b93a94ed2ac63524a933ec0caf5af00
Gabriel Diniz <gabriel.sundiniz@opentext.com>
2024-11-13 13:34:30 -0500

    [update] commands.bat to include timer function

--- bin/commands.bat
+++ bin/commands.bat
@@ -11,6 +11,10 @@ echo  dependency-check
 echo    - Used for installing all required libraries
 echo.
 
+echo  timer <time-in-minutes>
+echo    - Set a timer that alerts you with a sound once its done
+echo.
+
 echo  example-command ^<parameter^>
 echo    - This command does something
 echo.
