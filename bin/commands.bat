c557975cb6b5f27c0237bfedc4ab4d393e6f7ef4
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-11-20 21:10:16 -0500

    [fix] handle special characters for timer

--- bin/commands.bat
+++ bin/commands.bat
@@ -1,17 +1,18 @@
 @echo off
 echo ================================================
-powershell -Command "Write-Host '          Welcome to Custom Scripts!' -ForegroundColor Cyan"
+powershell -NoProfile -Command "& {Write-Host '          Welcome to Custom Scripts!' -ForegroundColor Cyan}"
 echo ================================================
 
 echo.
 echo  Commands available:
 echo.
 
+echo.
 echo  config-libs
 echo    - Used for installing all required libraries
 echo.
 
-echo  timer <time-in-minutes>
+echo  timer ^<time-in-minutes^>
 echo    - Set a timer that alerts you with a sound once its done
 echo.
 
