46bf55d6f6f2f163a011954eef8323a264dc996e
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-01-10 20:59:35 -0500

    [update] add a catch error if parameter is not passed

--- bin/open.bat
+++ bin/open.bat
@@ -1,13 +1,22 @@
 @echo off
 
-@REM Default directory for your projects
+:: Check if a parameter is passed
+if "%1"=="" (
+    echo You must provide the name of the GitHub repository.
+    echo Usage: open.bat ^<github-repo^>
+    exit /b 1
+)
+
+:: Default directory for your projects
 set "base_directory=C:\Users\gabri\Desktop\Code"
 set "repo_name=%1"
 set "repo_path=%base_directory%\%repo_name%"
 
+:: Check if the repository exists
 if exist "%repo_path%" (
     cd "%repo_path%"
     code .
 ) else (
-    echo Repository "%repo_name%" does not exist in %base_directory%.
+    echo Error: Repository "%repo_name%" does not exist in %base_directory%.
+    exit /b 1
 )
